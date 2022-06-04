3d398db25cd3a3769da09d9204a4a12184dd2da0
senbiki <akifumi.senbiki.1209@gmail.com>
2022-06-04 20:32:57 +0900

    chrome 拡張でDOM ドキュメントを操作できるか検証

--- block_shadow.js
+++ block_shadow.js
@@ -1,7 +1,5 @@
-// 全タグ要素にあたっているbox-shadowプロパティを無効化する拡張
-// window.addEventListener("load", function (e) {
-
-
+// プログラムの実行をdocument_endに指定すると
+// windowのloadイベントを待つ必要はなさそう
 function scanNodes(element) {
   let speech = new SpeechSynthesisUtterance();
   speech.lang = 'ja-JP';
@@ -12,7 +10,7 @@ function scanNodes(element) {
     let temp = element.textContent.trim();
     if (temp.length > 0) {
       speech.text = temp;
-      console.log(temp);
+      // console.log(temp);
       // 読み上げ処理
       // window.speechSynthesis.speak(speech);
     }
@@ -34,7 +32,20 @@ function scanNodes(element) {
 let body = document.getElementsByTagName("body");
 
 scanNodes(body[0]);
-// })
 
 
-
+// -------------------------------------------------
+// chrome拡張機能でDOMドキュメントを操作する検証
+// -------------------------------------------------
+let _ = function () {
+  let prompt = document.createElement("div");
+  let message = document.createTextNode("テキストを作成")
+  prompt.appendChild(message);
+  prompt.style.position = "fixed";
+  prompt.style.top = "0px";
+  prompt.style.left = "0px";
+  prompt.style.backgroundColor = "#DDD";
+  prompt.style.zIndex = "100000";
+  document.getElementsByTagName("body")[0].appendChild(prompt);
+}
+_ ();
