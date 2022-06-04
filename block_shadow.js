15fc71c6d0ae455e72c30e02dcd95267786e5579
Senbiki-Akifumi <a-senbiki@sunmark.bz>
2022-06-04 17:36:18 +0900

    テキストを抽出

--- block_shadow.js
+++ block_shadow.js
@@ -2,23 +2,38 @@
 // window.addEventListener("load", function (e) {
 
 
-  function scanNodes(element) {
+function scanNodes(element) {
+  let speech = new SpeechSynthesisUtterance();
+  speech.lang = 'ja-JP';
 
-    if (element.nodeName !== "#text" && element.nodeName !== "#comment") {
-      if (element.style && element.style.boxShadow) {
-        element.style.boxShadow = "";
-      }
-      element.style.boxShadow = "none";
+  if (element.tagName === "SCRIPT") {
+    // scriptタグはスルーする
+  } else if (element.nodeName === "#text" && element.tagName !== "SCRIPT") {
+    let temp = element.textContent.trim();
+    if (temp.length > 0) {
+      speech.text = temp;
+      console.log(temp);
+      // 読み上げ処理
+      // window.speechSynthesis.speak(speech);
+    }
+  } else if (element.nodeName === "#comment") {
+    // コメントはスルーする
+    // console.log("#comment ==> ", element);
+  } else {
+    if (element.style && element.style.boxShadow) {
+      element.style.boxShadow = "";
+    }
+    element.style.boxShadow = "none";
 
-      for (let i = 0; i < element.childNodes.length; i++) {
-        scanNodes(element.childNodes[i]);
-      }
+    for (let i = 0; i < element.childNodes.length; i++) {
+      scanNodes(element.childNodes[i]);
     }
   }
+}
 
-  let body = document.getElementsByTagName("body");
+let body = document.getElementsByTagName("body");
 
-  scanNodes(body[0]);
+scanNodes(body[0]);
 // })
 
 
